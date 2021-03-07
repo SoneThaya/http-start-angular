740acfa93ecee76254563a33ff905009f7e6ac7f
Sone <sthaya@gmail.com>
2021-03-06 18:09:34 -0800

    auth interceptor

--- http-01-start/src/app/app.module.ts
+++ http-01-start/src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { AuthInterceptorService } from "./auth-intercepter.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 // NEED HttpClientModule TO MAKE API REQUEST
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 
@@ -11,7 +12,13 @@ import { AppComponent } from "./app.component";
   declarations: [AppComponent],
   // DON'T FORGET TO IMPORT IT HERE
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
